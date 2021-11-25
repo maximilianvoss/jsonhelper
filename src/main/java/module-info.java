3fd67385594038378f2ef5145c0127cd7d7211c2
Maximilian Voss <voss@adobe.com>
2021-11-25 11:03:23 +0100

    In which our hero release version 1.1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ open module rocks.voss.jsonhelper {
     requires transitive com.fasterxml.jackson.core;
     requires transitive com.fasterxml.jackson.annotation;
     requires transitive com.fasterxml.jackson.databind;
+    requires transitive org.apache.logging.log4j;
 
     exports rocks.voss.jsonhelper;
 }
--- src/main/java/rocks/voss/jsonhelper/JSONHelper.java
+++ src/main/java/rocks/voss/jsonhelper/JSONHelper.java
@@ -4,36 +4,55 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class JSONHelper {
+    private static final Logger log = LogManager.getLogger(JSONHelper.class);
+
     public static <T> T createBean(Class<T> clazz, InputStream stream) throws IOException {
-        String json = IOUtils.toString(stream, "UTF-8");
-        return createBean(clazz, json);
+        String json = null;
+        try {
+            json = IOUtils.toString(stream, "UTF-8");
+            return createBean(clazz, json);
+        } catch (IOException e) {
+            log.error("JSON: {} ", json, e);
+            throw e;
+        }
     }
 
     public static <T> T createBean(Class<T> clazz, String json) throws IOException {
-        ObjectMapper mapper = new ObjectMapper()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        log.debug("JSON: {} ", json);
+        try {
+            ObjectMapper mapper = new ObjectMapper()
+                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        JsonNode jsonNode = mapper.readValue(json, JsonNode.class);
-        return mapper.treeToValue(jsonNode, clazz);
+            JsonNode jsonNode = mapper.readValue(json, JsonNode.class);
+            return mapper.treeToValue(jsonNode, clazz);
+        } catch (IOException e) {
+            log.error("JSON: {}", json, e);
+            throw e;
+        }
     }
 
     public static <T> T createBean(Class<T> clazz, Object obj) throws IOException {
         String jsonString = getJsonString(obj);
+        log.debug("JSON: {}", jsonString);
         return createBean(clazz, jsonString);
+    }
+
+    public static String getJsonString(Object object) {
+        String jsonString = getJson(object).toString();
+        log.debug("JSON: {}", jsonString);
+        return jsonString;
     }
 
     private static JsonNode getJson(Object object) {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode returnJson = mapper.valueToTree(object);
         return returnJson;
-    }
-
-    public static String getJsonString(Object object) {
-        return getJson(object).toString();
     }
 }
